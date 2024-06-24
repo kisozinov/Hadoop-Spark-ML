843cc743a68137852de075394fb564ec29a25384
Kirill Sozinov <cirill2001@bk.ru>
2024-06-24 11:39:50 +0300

    multiple experiments

--- spark_app.py
+++ spark_app.py
@@ -3,6 +3,7 @@ import psutil
 import logging
 import os
 import json
+import numpy as np
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import StringIndexer, Tokenizer, StopWordsRemover, HashingTF, IDF
 from pyspark.ml.classification import LogisticRegression
@@ -48,98 +49,107 @@ def main(num_nodes: int, optimize: bool):
 
     spark.sparkContext.setLogLevel("ERROR")
 
-    cumulative_times = []
-    total_mems = []
-    total_time = 0
+    all_times, all_mems = [], []
 
-    @measure_time
-    def load_data():
-        df = spark.read.parquet("hdfs://namenode:9001/user/root/data/Musical_Instruments_5.parquet")
-        return df.dropna(subset=['reviewText']).drop("vote").drop("style").drop("image")
+    for _ in range(20): # num experiments
+        cumulative_times = []
+        total_mems = []
+        total_time = 0
 
-    df = load_data()
-    if optimize:
-        if int(num_nodes) > 1:
-            df = df.repartition(int(num_nodes)).cache()
-        else:
-            df = df.cache()
-    df.show()
-    logger.info("Data loading is done.\n")
-    # df.printSchema()
+        @measure_time
+        def load_data():
+            df = spark.read.parquet("hdfs://namenode:9001/user/root/data/Musical_Instruments_5.parquet")
+            return df.dropna(subset=['reviewText']).drop("vote").drop("style").drop("image")
 
-    if optimize:
-        rdd = spark.sparkContext.parallelize(df.collect(), numSlices=10)
-        df = spark.createDataFrame(rdd, schema=df.schema)
-        df.cache()
+        df = load_data()
+        if optimize:
+            if int(num_nodes) > 1:
+                df = df.repartition(int(num_nodes)).cache()
+            else:
+                df = df.cache()
+        df.show()
+        logger.info("Data loading is done.\n")
+        # df.printSchema()
 
-    @measure_time
-    def tokenize_data(df):
-        tokenizer = Tokenizer(inputCol="reviewText", outputCol="words")
-        return tokenizer.transform(df)
+        if optimize:
+            rdd = spark.sparkContext.parallelize(df.collect(), numSlices=10)
+            df = spark.createDataFrame(rdd, schema=df.schema)
+            df.cache()
 
-    tokenized_df = tokenize_data(df)
-    logger.info("Tokenization is done.\n")
+        @measure_time
+        def tokenize_data(df):
+            tokenizer = Tokenizer(inputCol="reviewText", outputCol="words")
+            return tokenizer.transform(df)
 
-    @measure_time
-    def remove_stopwords(df):
-        remover = StopWordsRemover(inputCol="words", outputCol="filtered_words")
-        return remover.transform(df)
+        tokenized_df = tokenize_data(df)
+        logger.info("Tokenization is done.\n")
 
-    filtered_df = remove_stopwords(tokenized_df)
-    logger.info("Stopwords removing is done.\n")
+        @measure_time
+        def remove_stopwords(df):
+            remover = StopWordsRemover(inputCol="words", outputCol="filtered_words")
+            return remover.transform(df)
 
-    @measure_time
-    def apply_hashing_tf(df):
-        hashingTF = HashingTF(inputCol="filtered_words", outputCol="raw_features", numFeatures=10000)
-        return hashingTF.transform(df)
+        filtered_df = remove_stopwords(tokenized_df)
+        logger.info("Stopwords removing is done.\n")
 
-    hashed_df = apply_hashing_tf(filtered_df)
-    logger.info("TF calculating is done.\n")
+        @measure_time
+        def apply_hashing_tf(df):
+            hashingTF = HashingTF(inputCol="filtered_words", outputCol="raw_features", numFeatures=10000)
+            return hashingTF.transform(df)
 
-    @measure_time
-    def apply_idf(df):
-        idf = IDF(inputCol="raw_features", outputCol="features")
-        idf_model = idf.fit(df)
-        return idf_model.transform(df)
+        hashed_df = apply_hashing_tf(filtered_df)
+        logger.info("TF calculating is done.\n")
 
-    rescaled_df = apply_idf(hashed_df)
-    logger.info("IDF calculating is done.\n")
+        @measure_time
+        def apply_idf(df):
+            idf = IDF(inputCol="raw_features", outputCol="features")
+            idf_model = idf.fit(df)
+            return idf_model.transform(df)
 
-    @measure_time
-    def index_labels(df):
-        label_indexer = StringIndexer(inputCol="overall", outputCol="label")
-        return label_indexer.fit(df).transform(df)
+        rescaled_df = apply_idf(hashed_df)
+        logger.info("IDF calculating is done.\n")
 
-    indexed_df = index_labels(rescaled_df)
-    logger.info("Label encoding is done.\n")
+        @measure_time
+        def index_labels(df):
+            label_indexer = StringIndexer(inputCol="overall", outputCol="label")
+            return label_indexer.fit(df).transform(df)
 
-    @measure_time
-    def train_model(df):
-        lr = LogisticRegression(featuresCol="features", labelCol="label", maxIter=10)
-        #pipeline = Pipeline(stages=[tokenizer, remover, hashingTF, idf, label_indexer, lr])
-        return lr.fit(df)
+        indexed_df = index_labels(rescaled_df)
+        logger.info("Label encoding is done.\n")
 
-    (train_df, test_df) = indexed_df.randomSplit([0.8, 0.2], seed=42)
-    if optimize:
-        train_df = train_df.cache()
-        test_df = test_df.cache()
-    model = train_model(train_df)
-    logger.info("Model training is done.\n")
+        @measure_time
+        def train_model(df):
+            lr = LogisticRegression(featuresCol="features", labelCol="label", maxIter=10)
+            #pipeline = Pipeline(stages=[tokenizer, remover, hashingTF, idf, label_indexer, lr])
+            return lr.fit(df)
 
-    @measure_time
-    def evaluate_model(model, df):
-        predictions = model.transform(df)
-        evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
-        accuracy = evaluator.evaluate(predictions)
-        logger.info(f"Model accuracy: {accuracy:.4f}")
-        return predictions
+        (train_df, test_df) = indexed_df.randomSplit([0.8, 0.2], seed=42)
+        if optimize:
+            train_df = train_df.cache()
+            test_df = test_df.cache()
+        model = train_model(train_df)
+        logger.info("Model training is done.\n")
 
-    predictions = evaluate_model(model, test_df)
-    logger.info("Model evaluation is done.\n")
-    predictions.select("reviewText", "overall", "prediction").show()
+        @measure_time
+        def evaluate_model(model, df):
+            predictions = model.transform(df)
+            evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
+            accuracy = evaluator.evaluate(predictions)
+            logger.info(f"Model accuracy: {accuracy:.4f}")
+            return predictions
 
-    logger.info(f"Cumulative times: {cumulative_times}")
-    logger.info(f"Memory consumtion per step: {total_mems}")
+        predictions = evaluate_model(model, test_df)
+        logger.info("Model evaluation is done.\n")
+        predictions.select("reviewText", "overall", "prediction").show()
+
+        logger.info(f"Cumulative times: {cumulative_times}")
+        logger.info(f"Memory consumtion per step: {total_mems}")
+        
+        all_times.append(cumulative_times)
+        all_mems.append(total_mems)
+
+    cumulative_times = list(np.mean(np.array(all_times), axis=0))
+    total_mems = list(np.mean(np.array(all_mems), axis=0))
 
     metrics = {
             "time": cumulative_times,
